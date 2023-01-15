c9b324d30c91779f89e02371b42b750fd80d8943
1Suro1 <timu.zaxarov@gmail.com>
2023-01-15 14:19:12 +0900

    delete cloud command library

--- api/src/main/kotlin/eu/suro/VelocityMain.kt
+++ api/src/main/kotlin/eu/suro/VelocityMain.kt
@@ -1,26 +1,12 @@
 package eu.suro
 
-import cloud.commandframework.annotations.AnnotationParser
-import cloud.commandframework.execution.AsynchronousCommandExecutionCoordinator
-import cloud.commandframework.kotlin.coroutines.annotations.installCoroutineSupport
-import cloud.commandframework.meta.SimpleCommandMeta
-import cloud.commandframework.velocity.CloudInjectionModule
-import cloud.commandframework.velocity.VelocityCommandManager
-import com.github.shynixn.mccoroutine.velocity.MCCoroutine
 import com.github.shynixn.mccoroutine.velocity.SuspendingPluginContainer
-import com.github.shynixn.mccoroutine.velocity.registerSuspend
 import com.google.inject.Inject
-import com.google.inject.Injector
-import com.google.inject.Key
-import com.google.inject.TypeLiteral
-import com.velocitypowered.api.command.CommandSource
 import com.velocitypowered.api.event.Subscribe
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent
-import com.velocitypowered.api.plugin.Plugin
 import com.velocitypowered.api.plugin.PluginContainer
 import com.velocitypowered.api.plugin.annotation.DataDirectory
 import com.velocitypowered.api.proxy.ProxyServer
-import eu.suro.command.TestCommand
 import eu.suro.grpc.GRPChannel
 import eu.suro.messanger.MessangerInit
 import eu.suro.messanger.listener.VelocityMessangerListener
@@ -29,7 +15,6 @@ import eu.suro.redis.channel.RedisPacketListener
 import eu.suro.redis.platform.velocity.VelocityRedisEventImpl
 import eu.suro.utils.Log
 import java.nio.file.Path
-import java.util.function.Function
 
 //@Plugin(id = "hyneo", name = "HyNeoApi", version = "1.0")
 class VelocityMain @Inject constructor(suspendingPluginContainer: SuspendingPluginContainer, @DataDirectory val dataDirectory: Path) {
