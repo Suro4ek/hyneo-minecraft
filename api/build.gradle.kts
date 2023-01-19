a2f3202566eb8a47193c514b1c1180c8a38f6022
suro suro <bogayt@gmail.com>
2023-01-19 13:50:59 +0900

    update command-library

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -137,7 +137,7 @@ dependencies {
 
     compileOnly(libs.velocity)
 
-    implementation("eu.suro.command:command-velocity:1.0.8")
+    implementation("eu.suro.command:command-velocity:1.0.9")
 
     compileOnly(spigot("1.15.2"))
 
