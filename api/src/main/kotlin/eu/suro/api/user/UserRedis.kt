2f8980a82d282d3bdd1d2a1136b1bf5f80d08dcd
1Suro1 <timu.zaxarov@gmail.com>
2023-01-28 23:46:59 +0900

    maybe fix

--- api/src/main/kotlin/eu/suro/api/user/UserRedis.kt
+++ api/src/main/kotlin/eu/suro/api/user/UserRedis.kt
@@ -7,18 +7,37 @@ import java.io.Serializable
 
 
 @REntity
-open class UserRedis constructor(
+open class UserRedis() {
     @RId
-    var id: String,
+    var id: String = "-1"
     @RIndex
-    var username: String,
-    var ip: String,
-    var prefix: String,
-    var last_server: String,
-    var register_ip: String,
-    var locale_id: Int,
-    var auth: Boolean,
-) {
+    var username: String = ""
+    var ip: String = ""
+    var prefix: String = ""
+    var last_server: String = ""
+    var register_ip: String = ""
+    var locale_id: Int = 0
+    var auth: Boolean = false
+
+    constructor(
+     id: String,
+     username: String,
+     ip: String,
+     prefix: String,
+     last_server: String,
+     register_ip: String,
+     locale_id: Int,
+     auth: Boolean
+    ) : this() {
+        this.id = id
+        this.username = username
+        this.ip = ip
+        this.prefix = prefix
+        this.last_server = last_server
+        this.register_ip = register_ip
+        this.locale_id = locale_id
+        this.auth =auth
+    }
 
     override fun toString()
     = """{
